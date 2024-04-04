43db57653a7ae897e06ddf6629c1b8613cdfb9d2
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2024-04-04 09:44:41 -0400

    Updated class and Notes.

--- src/main/java/com/kuebiko/amazonemployee/dto_entity/ScheduleDTO.java
+++ src/main/java/com/kuebiko/amazonemployee/dto_entity/ScheduleDTO.java
@@ -1,4 +1,48 @@
 package com.kuebiko.amazonemployee.dto_entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Table(schema = "amazon_company", name = "schedule_calculate")
+@Data
+@Getter
+@Setter
+@Entity
+
 public class ScheduleDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long scheduleID;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    public void totalDaysWork(){
+        if (startDate != null && endDate != null) {
+            //totalDaysWork = ChronoUnit.DAYS.between(startDate, endDate) + 1;
+        } else {
+            //totalDaysWork = 0L; // Or any other default value indicating invalid dates
+        }
+    }
+
+    private LocalTime punchIn;
+    private LocalTime punchOut;
+
+    public BigDecimal TotalHoursWorked(){
+        if (punchIn !=null && punchOut !=null){
+            Duration duration = Duration.between(punchIn, punchOut);
+            Long seconds = duration.getSeconds();
+            BigDecimal hours = BigDecimal.valueOf(seconds).divide(BigDecimal.valueOf(3600), 2, BigDecimal.ROUND_HALF_UP);
+            return  hours;
+        }else {
+            return BigDecimal.ZERO;
+        }
+    }
 }
--- src/main/java/com/kuebiko/amazonemployee/dto_entity/WagesDTO.java
+++ src/main/java/com/kuebiko/amazonemployee/dto_entity/WagesDTO.java
@@ -1,4 +1,28 @@
 package com.kuebiko.amazonemployee.dto_entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Table(schema = "amazon_company", name = "wages_calculation")
+@Data
+@Getter
+@Setter
+@Entity
+
 public class WagesDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long wagesID;
+    private BigDecimal hourlyRate;
+    private Float totalWorkedTime;
+    private Float bonusAmount;
+    private Float overTimePay;
+    private BigDecimal regularWages;
+    private Float totalWages;
 }
